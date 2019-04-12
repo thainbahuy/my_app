e27131e31c1588a23c0d2250de021ec79d577ab2
thainbahuy <thainbahuy@gmail.com>
2019-04-12 09:06:49 +0700

    add abtractclass

--- app/Contracts/Repositories.php
+++ app/Contracts/Repositories.php
@@ -1,20 +1,23 @@
 <?php
 namespace App\Contracts\Repositories;
 
-interface AccountRepositoryInterface
+interface AbtractRepository
 {
     public function getAllData();
     public function deleteById($id);
+}
+interface AccountRepositoryInterface extends AbtractRepository
+{
+
     public function addNewData($username, $password);
     public function getDataById($id);
     public function updateData($id, $username, $password);
 
 }
 
-interface ProductRepositoryInterface
+interface ProductRepositoryInterface extends AbtractRepository
 {
-    public function getAllData();
-    public function deleteById($id);
+
     public function addNewData($dataProduct);
     // public function getDataById();
     // public function updateData ();
