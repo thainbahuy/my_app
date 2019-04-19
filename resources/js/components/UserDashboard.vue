c24b1c12b9ce389c6afd7cdc57fb2eb44b49c732
thainbahuy <thainbahuy@gmail.com>
2019-04-19 15:59:21 +0700

    add UserDashboard.vue

---
+++ resources/js/components/UserDashboard.vue
@@ -0,0 +1,38 @@
+<template>
+    <div class="user-dashboard">
+       <div class="list-user-comp">
+           <!-- Listen event 'userSelected' emitted from child and process it using 'childrenSelectUser' -->
+            <!--@<Tên sự kiện của con gửi lên>=<Hàm xử lý ở cha>-->
+           <ListUser @userSelected = "childrenSelectUser"></ListUser>
+       </div>
+        <div class="user-detail-comp">
+            <!-- pass param 'userSelectedFromChild', child will use props 'userDetailInfo' -->
+            <UserDetail :userDetailInfo="userSelectedFromChild"></UserDetail>
+        </div>
+    </div>
+</template>
+
+<script>
+    import ListUser from './ListUser.vue'
+    import UserDetail from './UserDetail.vue'
+    export default {
+        components : {
+            ListUser,
+            UserDetail
+        },
+        data(){
+            return {
+                userSelectedFromChild: {}
+            }
+        },
+        methods : {
+            childrenSelectUser (user){
+                console.log(user)
+                this.userSelectedFromChild = user
+            }
+        }
+    }
+</script>
+
+<style lang="scss" scoped>
+</style>
